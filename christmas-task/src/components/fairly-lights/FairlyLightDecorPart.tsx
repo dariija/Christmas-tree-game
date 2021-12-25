2f07cde1a624ef1e8e15f67178860c6eccde42ba
dariija <darija.bakanova@gmail.com>
2021-12-25 08:49:03 +0300

    feat: add styles

--- christmas-task/src/components/fairly-lights/FairlyLightDecorPart.tsx
+++ christmas-task/src/components/fairly-lights/FairlyLightDecorPart.tsx
@@ -6,10 +6,21 @@ type Props = {
     colour: string;
 };
 
+type ColourChart = {
+    [index: string]: string;
+};
+
 export default function FairlyLightDecorPart({ bulbQuantity, interval, colour }: Props) {
     const startAngle = 70;
     const endAngle = 110;
     const angleStep = (endAngle - startAngle) / (bulbQuantity - 1);
+
+    const colourChart: ColourChart = {
+        red: '#ff4c4c',
+        yellow: '#ffff47',
+        green: '#61f361',
+        blue: '#0e9bff',
+    };
 
     return (
         <ul className="fairly-light-decor-part">
@@ -20,7 +31,7 @@ export default function FairlyLightDecorPart({ bulbQuantity, interval, colour }:
                     }`}
                     style={{
                         transform: `rotate(${startAngle + index * angleStep}deg) translate(${interval}px)`,
-                        backgroundColor: `${colour === 'multi' ? '' : colour}`,
+                        backgroundColor: `${colour === 'multi' ? '' : colourChart[colour]}`,
                     }}
                     key={index.toString()}
                 />
--- christmas-task/src/components/fairly-lights/FairyLights.tsx
+++ christmas-task/src/components/fairly-lights/FairyLights.tsx
@@ -29,21 +29,23 @@ export default function FairyLights({ settings }: Props) {
         <div className="fairy-lights">
             <p className="fairy-lights__title">Fairy-light</p>
             <div className="fairy-lights__content">
-                {coloursLights.map((colour, index) => (
-                    <FairlyLight colour={colour} key={index.toString()} settings={settings} />
-                ))}
-            </div>
+                <div className="fairy-lights__lights">
+                    {coloursLights.map((colour, index) => (
+                        <FairlyLight colour={colour} key={index.toString()} settings={settings} />
+                    ))}
+                </div>
 
-            <label className="switch" htmlFor="switch">
-                <input
-                    className="switch__checkbox"
-                    type="checkbox"
-                    id="switch"
-                    onChange={handlerChange}
-                    checked={switchBtn}
-                />
-                <span className="switch__slider" />
-            </label>
+                <label className="switch" htmlFor="switch">
+                    <input
+                        className="switch__checkbox"
+                        type="checkbox"
+                        id="switch"
+                        onChange={handlerChange}
+                        checked={switchBtn}
+                    />
+                    <span className="switch__slider" />
+                </label>
+            </div>
         </div>
     );
 }
